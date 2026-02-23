9c4e8cca750710f7df3a935b877b9e205ecca543
KoMacOS <nkakio88@gmail.com>
2026-02-23 12:36:48 -0800

    final commit for Machine Problem 2 p1

--- src/main/java/org/groupid/Battle.java
+++ src/main/java/org/groupid/Battle.java
@@ -2,6 +2,7 @@ package org.groupid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import java.util.Random;
 
 @Component
 public class Battle {
@@ -19,9 +20,66 @@ public class Battle {
     }
 
     public void fight() {
+        System.out.println("    BATTLE START.   ");
         System.out.print(battleground.getBattleGroundDesc() + ", ");
-        System.out.println(character1.getName() + " and " + character2.getName() + " Begin to fight.");
-        character1.useFightingPower();
-        character2.useFightingPower();
+        System.out.println(character1.getName() + " and " + character2.getName() + " begin to fight.");
+        System.out.println(character1.getName() + " starting HP: " + character1.getHealth());
+        System.out.println(character2.getName() + " starting HP: " + character2.getHealth());
+        System.out.println("\n");
+
+        Random random = new Random();
+        Character firstAttacker;
+        Character secondAttacker;
+
+        if (random.nextBoolean()) {
+            firstAttacker = character1;
+            secondAttacker = character2;
+        } else {
+            firstAttacker = character2;
+            secondAttacker = character1;
+        }
+
+        System.out.println(firstAttacker.getName() + " attacks first!\n");
+
+        int round = 1;
+
+        while (firstAttacker.isAlive() && secondAttacker.isAlive()) {
+            System.out.println("    Round " + round + "    ");
+
+            firstAttacker.useFightingPower();
+            double damage1 = firstAttacker.getStrength();
+            if (random.nextInt(100) < 10) {
+                damage1 = damage1 * 1.10;
+                System.out.println(" CRITICAL HIT!");
+            }
+            secondAttacker.takeDamage(damage1);
+            System.out.println("    " + secondAttacker.getName() + " takes " + String.format("%.1f", damage1) + " damage. HP remaining: " + String.format("%.1f", secondAttacker.getHealth()));
+
+            if (!secondAttacker.isAlive()) {
+                System.out.println("\n " + secondAttacker.getName() + " has been defeated! " + firstAttacker.getName() + " wins!");
+                break;
+            }
+
+            secondAttacker.useFightingPower();
+            double damage2 = secondAttacker.getStrength();
+            if (random.nextInt(100) < 10) {
+                damage2 = damage2 * 1.10;
+                System.out.println(" CRITICAL HIT!");
+            }
+            firstAttacker.takeDamage(damage2);
+            System.out.println("    " + firstAttacker.getName() + " takes " + String.format("%.1f", damage2) + " damage. HP remaining: " + String.format("%.1f", firstAttacker.getHealth()));
+
+            if (!firstAttacker.isAlive()) {
+                System.out.println("\n " + firstAttacker.getName() + " has been defeated! " + secondAttacker.getName() + " wins!");
+                break;
+            }
+
+            firstAttacker.increaseStrength();
+            secondAttacker.increaseStrength();
+
+            System.out.println(" Both fighters grow 10% stronger!");
+            System.out.println();
+            round++;
+        }
     }
 }
--- src/main/java/org/groupid/Character.java
+++ src/main/java/org/groupid/Character.java
@@ -10,9 +10,26 @@ public class Character {
         System.out.println(name + " is " + fightingPower.getFightingPowerDesc());
     }
 
+    public void takeDamage(double damage) {
+        this.health -= damage;
+        if (this.health < 0) {
+            this.health = 0;
+        }
+    }
+
+    public void increaseStrength() {
+        this.strength = this.strength * 1.10;
+    }
+
+    public boolean isAlive() {
+        return this.health > 0;
+    }
+
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
+    public double getHealth() { return health; }
     public void setHealth(double health) { this.health = health; }
+    public double getStrength() { return strength; }
     public void setStrength(double strength) { this.strength = strength; }
     public void setFightingPower(FightingPower fightingPower) { this.fightingPower = fightingPower; }
 }
